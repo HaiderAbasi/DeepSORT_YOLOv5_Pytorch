32bbc0d06c79e9645ea374499bdf950ce114b7b3
Haider Abasi <haiderabbasi333@gmail.com>
2023-02-22 23:07:21 +0500

    Summary of the change made.
    Project: PeopleTrackr Implemented with the following features,
    * People counter, (closes #5)
    * Displays tracked people trajectories, (closes #6)
    * Focus on a suspicious individual and write track data to .csv, (closes #7)

--- utils_ds/draw.py
+++ utils_ds/draw.py
@@ -1,7 +1,28 @@
 import numpy as np
 import cv2
+import csv
+import datetime
+import atexit
+import os
 
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+
+def write_to_csv(file_path, bbox, class_name):
+    # Create header if file is new
+    is_new_file = not os.path.isfile(file_path)
+    with open(file_path, mode='a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        if is_new_file:
+            writer.writerow(['Timestamp', 'BBox', 'Class'])
+        
+        # Write data to file
+        current_time = datetime.datetime.now()
+        writer.writerow([current_time.strftime("%Y-%m-%d %H:%M:%S"), bbox, class_name])
+    
+    # Close file when program terminates
+    @atexit.register
+    def close_csv_file():
+        csv_file.close()
 
 
 def compute_color_for_labels(label):
@@ -11,8 +32,27 @@ def compute_color_for_labels(label):
     color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
     return tuple(color)
 
+def overlay_on_image(image,clr_trails):
+    h, w = clr_trails.shape[:2]
+    overlay = np.zeros((h, w, 3), dtype=np.uint8)
+    
+    gray = cv2.cvtColor(clr_trails,cv2.COLOR_BGR2GRAY)
+    mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)[1]
+    inverted_mask = cv2.bitwise_not(mask)
+    image = cv2.bitwise_and(image, image, mask=inverted_mask)
+    overlay = cv2.bitwise_and(clr_trails, clr_trails, mask=mask)
+    image = cv2.bitwise_or(overlay, image)
+    return image
 
-def draw_boxes(img, bbox, identities=None, offset=(0,0)):
+def inc_int(color,factor = 1.4):
+    b,g,r = color
+    new_b = b*factor if b*factor < 255 else 255
+    new_g = g*factor if g*factor < 255 else 255
+    new_r = r*factor if r*factor < 255 else 255
+    return (new_b,new_g,new_r)
+
+
+def draw_boxes(img, bbox, identities=None, offset=(0,0),mask = None, trajectories = None,id_to_track = None,t_classes = None,categories = None):
     for i,box in enumerate(bbox):
         x1,y1,x2,y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -22,60 +62,35 @@ def draw_boxes(img, bbox, identities=None, offset=(0,0)):
         # box text and bar
         id = int(identities[i]) if identities is not None else 0    
         color = compute_color_for_labels(id)
+        
+        bbox_thickness = 3
+        draw_trajectory = True
+        if id_to_track and id == id_to_track:
+            color = (0,255,0)
+            write_to_csv("john_doe.csv",box,t_classes[id])
+        elif id_to_track:
+            color = (0,0,40)
+            bbox_thickness = 1
+            draw_trajectory = False
+        
+        
         label = '{}{:d}'.format("", id)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
-        cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
+        label_fscale = 2
+        if categories:
+            # If categories are avaible display them along the tracked object
+            label = categories[t_classes[i]]
+            label_fscale = 1.5
+        
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, label_fscale , 2)[0]
+        cv2.rectangle(img,(x1, y1),(x2,y2),color,bbox_thickness)
         cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
-        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, label_fscale, [255,255,255], 2)
+        
+        if draw_trajectory and len(trajectories[id])==2:
+            cv2.line(mask,trajectories[id][0],trajectories[id][1],inc_int(color),3)
+        img = overlay_on_image(img,mask)
+        
     return img
-
-def putText(img, text, font=cv2.FONT_HERSHEY_PLAIN, font_scale=1, color=(0, 0, 255), thickness=1, margin=10, pos='top-right', bg_color=None):
-    """
-    Displays text on an image with an adjustable margin.
-
-    Parameters:
-    img (numpy.ndarray): The input image.
-    text (str): The text to display.
-    font (int): The font type to use (default: cv2.FONT_HERSHEY_PLAIN).
-    font_scale (float): The font scale factor (default: 1).
-    color (tuple): The text color in BGR format (default: (0, 0, 255)).
-    thickness (int): The thickness of the text (default: 1).
-    margin (int): The margin between the text and the edge of the image (default: 10).
-    pos (str): The position of the text. Can be 'top-left', 'top-right', 'bottom-left', or 'bottom-right' (default: 'top-right').
-    """
-
-    # Get the size of the text
-    text_size, _ = cv2.getTextSize(text, font, font_scale, thickness=thickness)
-
-    # Define the position of the text
-    if pos == 'top-left':
-        text_x = margin
-        text_y = margin + text_size[1]
-    elif pos == 'top-right':
-        text_x = img.shape[1] - text_size[0] - margin
-        text_y = margin + text_size[1]
-    elif pos == 'bottom-left':
-        text_x = margin
-        text_y = img.shape[0] - margin
-    elif pos == 'bottom-right':
-        text_x = img.shape[1] - text_size[0] - margin
-        text_y = img.shape[0] - margin
-    else:
-        raise ValueError("Invalid position. Position should be 'top-left', 'top-right', 'bottom-left', or 'bottom-right'.")
-    
-    # Draw the text with or without background color
-    if bg_color:
-        # Calculate the background rectangle size and position
-        bg_rect_width = text_size[0] + margin * 2
-        bg_rect_height = text_size[1] + margin * 2
-        bg_rect_x = text_x - margin
-        bg_rect_y = text_y - text_size[1] - margin
-
-        # Draw the background rectangle
-        cv2.rectangle(img, (bg_rect_x, bg_rect_y), (bg_rect_x + bg_rect_width, bg_rect_y + bg_rect_height), bg_color, thickness=-1)
-
-    # Draw the text on the image
-    cv2.putText(img, text, (text_x, text_y), font, font_scale, color, thickness)
 
 
 if __name__ == '__main__':
